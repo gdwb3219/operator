5eb0a3212fbb05c60997ffa9ee34e4d5d252543e
gdwb3219 <gdwb3219@gmail.com>
2022-01-17 22:36:50 +0900

    Fast Campus JS Operator Practice

---
+++ main.js
@@ -0,0 +1,39 @@
+// 산술 연산자 (arithmetic operator)
+
+// console.log(1 + 2)
+// console.log(5 - 7)
+// console.log(3 * 4)
+// console.log(10 / 2)
+// console.log(10 % 3)
+
+// 할당 연산자 (assignment operator)
+
+// let a = 2
+// a += 1
+// a -= 1
+// a *= 2
+// a /= 1
+// a %= 1
+
+// console.log(a)
+
+// 비교 연산자
+
+// const a = 1
+// const b = 3
+
+// console.log(a === b)
+
+// function isEqual(x, y) {`
+//   return x === y`
+// }
+
+// console.log(isEqual(1, 1))
+// console.log(isEqual(2, '2'))
+
+// console.log(a !== b)
+
+// 삼항 연산자 
+
+const a = 1 > 2
+console.log(a ? '참' : '거짓')
