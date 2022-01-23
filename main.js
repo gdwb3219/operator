910b838b2f1894c35042682f3e96c7894cd33b99
gdwb3219 <gdwb3219@gmail.com>
2022-01-23 21:49:29 +0900

    add operator git

--- main.js
+++ main.js
@@ -1,39 +1,77 @@
-// 산술 연산자 (arithmetic operator)
+// import random from './getRandom'
 
-// console.log(1 + 2)
-// console.log(5 - 7)
-// console.log(3 * 4)
-// console.log(10 / 2)
-// console.log(10 % 3)
+// const a = random()
 
-// 할당 연산자 (assignment operator)
-
-// let a = 2
-// a += 1
-// a -= 1
-// a *= 2
-// a /= 1
-// a %= 1
-
-// console.log(a)
-
-// 비교 연산자
-
-// const a = 1
-// const b = 3
-
-// console.log(a === b)
-
-// function isEqual(x, y) {`
-//   return x === y`
+// switch (a) {
+//   case 0:
+//     console.log('a is 0')
+//     break
+//   case 2:
+//     console.log('a is 2')
+//     break
+//   case 4:
+//     console.log('a is 4')
+//     break
+//   case 6:
+//     console.log('a is 6')
+//     break
+//   default:
+//     console.log('rest...')
 // }
 
-// console.log(isEqual(1, 1))
-// console.log(isEqual(2, '2'))
+// if (a === 0) {
+//   console.log('a is 0')
+// } else if (a == 2) {
+//   console.log('a is 2')
+// } else if (a == 4) {
+//   console.log('a is 4')
+// } else if (a == 6) {
+//   console.log('a is 6')
+// } else {
+//   console.log('rest...')
+// }
 
-// console.log(a !== b)
+// 반복문 (For Statement)
+// for (시작조건; 종료 조건; 변화 조건) {}
 
-// 삼항 연산자 
+// const ulEl = document.querySelector('ul')
 
-const a = 1 > 2
-console.log(a ? '참' : '거짓')
+// for (let i = 0; i < 10; i += 1) {
+//   const li = document.createElement('li')
+//   li.textContent = `list-${i + 1}`
+//   if ((i+1) % 2 === 0) {
+//     li.addEventListener('click', function () {
+//       console.log(li.textContent)
+//     })
+//   }
+//   ulEl.appendChild(li)
+// }
+
+// 변수 유효범위 (Variable Scope)
+// var (x) , let, const
+
+// function scope() {
+//   if (true) {
+//     const a = 123
+//     console.log(a)
+//   }
+// }
+// scope()
+
+// 복습
+
+// function sum(x, y) {
+//   console.log(arguments)
+//   return arguments[0] + arguments[1]
+// }
+// console.log(sum(7, 3))
+
+const heropy = {
+  firstName: 'Heropy',
+  lastName: 'Park',
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+console.log(heropy)
