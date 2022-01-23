bc63a1746387f8c447442638c6ab60eb328ceaee
gdwb3219 <gdwb3219@gmail.com>
2022-01-23 23:33:05 +0900

    understand the arrow function of this method

--- main.js
+++ main.js
@@ -66,12 +66,101 @@
 // }
 // console.log(sum(7, 3))
 
-const heropy = {
-  firstName: 'Heropy',
-  lastName: 'Park',
-  getFullName: function () {
-    return `${this.firstName} ${this.lastName}`
+
+// # 생성자 함수
+// const heropy = {
+//   firstName: 'Heropy',
+//   lastName: 'Park',
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+
+// console.log(heropy.getFullName())
+
+// const amy = {
+//   firstName: 'Amy',
+//   lastName: 'Clarke',
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+// console.log(amy.getFullName())
+
+// const neo = {
+//   firstName: 'Neo',
+//   lastName: 'Smith',
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+// console.log(neo.getFullName())
+
+// function User(first, last) {
+//   this.firstName = first
+//   this.lastName = last
+// }
+// User.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`
+// }
+
+// const heropy = new User('Heropy', 'Park')
+// const amy = new User('Amy', 'Clarke')
+// const neo = new User('Neo', 'Smith')
+
+// console.log(heropy.getFullName())
+// console.log(amy.getFullName())
+// console.log(neo.getFullName())
+
+
+//  ## This
+
+// const heropy = {
+//   name: 'Heropy',
+//   normal: function () {
+//     console.log(this.name)
+//   },
+//   arrow: () => {
+//     console.log(this.name)
+//   }
+// }
+
+// heropy.normal()
+// heropy.arrow()
+
+// const amy = {
+//   name: 'Amy',
+//   normal: heropy.normal,
+//   arrow: heropy.arrow
+// }
+
+// amy.normal()
+// amy.arrow()
+
+
+// function User(name) {
+//   this.name = name
+// }
+// User.prototype.normal = function () {
+//   console.log(this.name)
+// }
+// User.prototype.arrow = () => {
+//   console.log(this.name)
+// }
+
+// const heropy = new User('Heropy')
+
+// heropy.normal()
+// heropy.arrow()
+
+
+const timer = {
+  name: 'Heropy!',
+  timeout: function () {
+    // setTimeout(함수, 시간)
+    setTimeout(() => {
+      console.log(this.name)
+    }, 2000)
   }
 }
-
-console.log(heropy)
+timer.timeout()
