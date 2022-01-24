a262564d9b20baec6c04074522ea71bd827285ac
gdwb3219 <gdwb3219@gmail.com>
2022-01-24 23:26:56 +0900

    Classes

--- main.js
+++ main.js
@@ -164,3 +164,19 @@ const timer = {
   }
 }
 timer.timeout()
+
+
+// ES6 Classes
+
+const heropy = {
+  name: 'Heropy',
+  normal: function () {
+    console.log(this.name)
+  },
+  arrow: () => {
+    console.log(this.name)
+  }
+}
+
+heropy.normal()
+heropy.arrow()
