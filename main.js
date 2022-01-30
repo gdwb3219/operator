84132defc491e0769277fdb4d250ad9e6330feff
gdwb3219 <gdwb3219@gmail.com>
2022-01-30 18:30:33 +0900

    understand data references

--- main.js
+++ main.js
@@ -456,7 +456,7 @@
 
 // 전개 연산자 (Spread)
 
-const fruits = ['Apple', 'Banana', 'Cherry', 'Orange']
+// const fruits = ['Apple', 'Banana', 'Cherry', 'Orange']
 // console.log(fruits)
 // console.log(...fruits)
 
@@ -472,31 +472,63 @@ const fruits = ['Apple', 'Banana', 'Cherry', 'Orange']
 
 // 축약형으로 반환
 // 1.
-let toObject1 = function(a, b, ...c) {
-  return {
-    a: a,
-    b: b,
-    c: c
-  }
-}
-console.log(toObject1(...fruits))
-// 2.
-let toObject2 = (a, b, ...c) => {
-  return {
-    a: a,
-    b: b,
-    c: c
-  }
-}
-console.log(toObject2(...fruits))
-// 3.
-let toObject3 = (a, b, ...c) => {
-  return {a, b, c}
-}
-console.log(toObject3(...fruits))
-// 4.
-let toObject4 = (a, b, ...c) => {a, b, c}
-console.log(toObject4(...fruits))
-// 5.
-let toObject5 = (a, b, ...c) => ({a, b, c})
-console.log(toObject5(...fruits))
+// let toObject1 = function(a, b, ...c) {
+//   return {
+//     a: a,
+//     b: b,
+//     c: c
+//   }
+// }
+// console.log(toObject1(...fruits))
+// // 2.
+// let toObject2 = (a, b, ...c) => {
+//   return {
+//     a: a,
+//     b: b,
+//     c: c
+//   }
+// }
+// console.log(toObject2(...fruits))
+// // 3.
+// let toObject3 = (a, b, ...c) => {
+//   return {a, b, c}
+// }
+// console.log(toObject3(...fruits))
+// // 4.
+// let toObject4 = (a, b, ...c) => {a, b, c}
+// console.log(toObject4(...fruits))
+// // 5.
+// let toObject5 = (a, b, ...c) => ({a, b, c})
+// console.log(toObject5(...fruits))
+
+
+// 데이터의 불변성
+
+// let a = 1
+// let b = 4
+// console.log(a, b, a===b)
+
+// b = a
+// console.log(a, b, a===b)
+
+// a = 7
+// console.log(a, b, a===b)
+
+// c = 1
+// console.log(b, c, b===c)
+
+
+// 참조형 데이터
+
+let a = {k: 1}
+let b = {k: 1}
+console.log(a, b, a === b)
+a.k = 7
+b = a
+console.log(a, b, a === b)
+a.k = 2
+console.log(a, b, a === b)
+let c = b
+console.log(a, b, c, a === c)
+a.k = 9
+console.log(a, b, c, a === c)
